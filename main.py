5f401372bcb5e66ccaaf8cc8b10873430aba6ac9
Molnar Peter Soma <h161736@stud.u-szeged.hu>
2024-10-17 18:00:43 +0200

    aggregaltbol elso sigmoidot kivenni probalo fuggveny hozzaadasa, neve:"subtractFirstSigmoidKnowingAlpha"
    Valamint decimalrol, visszaallas floatra (sokkal gyorsabb)

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import sigmoidFunctionLib as sfl
+
 import numpy as np
 import random
 
 plotmin = -100
-plotmax = 100
+plotmax = 400
+#params setup
+params = sfl.createParamsArray(1/4, 100, plotmin, plotmax, True)
+params = sfl.appendParams(params, 1/4, 140, plotmin, plotmax, False)
+params = sfl.appendParams(params, 1/4, 200, plotmin, plotmax, False)
+params = sfl.appendParams(params, 1/4, 250, plotmin, plotmax, True)
+#slice first sigmoid
+paramsminfirst = params[1:4]
 
-params = sfl.createParamsArray(1/4, -60, plotmin, plotmax, True)
-params = sfl.appendParams(params, 1/4, 0, plotmin, plotmax, False)
-#params = sfl.appendParams(params, 1/1, -45, plotmin, plotmax, True)
-#params = sfl.appendParams(params, 1/4, -40, plotmin, plotmax, False)
-#params = sfl.appendParams(params, 1/4, 0, plotmin, plotmax, True)
-#params = sfl.appendParams(params, 1/4, 5, plotmin, plotmax, False)
-
-
-
-
-
-#for i in range(-9, 40, 1):
- #   params = sfl.appendParams(params, 1/4, i, plotmin, plotmax, True)
-
+#params into sigmoids
 sigmoidok = sfl.sigmoidArray(params)
+sigmoidokminfirst = sfl.sigmoidArray(paramsminfirst)
 
+#sigmoids aggregated
 aggregate = sfl.sigmoidAggregate(sigmoidok)
+aggregateminfirst = sfl.sigmoidAggregate(sigmoidokminfirst)
 
-sfl.sigmoidArrayAndAggregatePlot(sigmoidok)
+#subtracting first sigmoid
+kivonas = sfl.subtractFirstSigmoidKnowingAlpha(aggregate, 1/4)
+
+#plots
+#sfl.aggregateShow(aggregate)
+#sfl.aggregateShow(aggregateminfirst)
+#sfl.aggregateShow(kivonas)
+#sfl.sigmoidArrayAndAggregatePlot(sigmoidokminfirst)
 
 import sys
 sys.exit()
--- sigmoidFunctionLib.py
+++ sigmoidFunctionLib.py
@@ -2,42 +2,37 @@ import random
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib as mpl
-from decimal import Decimal, getcontext
-from scipy.signal import argrelextrema
+from scipy.signal import argrelextrema, find_peaks
+from scipy.ndimage import gaussian_filter1d
 
-import pylab as pl
 
 mpl.use('TkAgg')
 
-getcontext().prec = 300
 
 
 #Takes in alpha,a and the x point to create the y value of a sigmoid function
 def sigmoidPoint(alpha, a, x):
-    return Decimal(0.99) / (1 + np.exp(-alpha * (x - a)))
+    return 0.99 / (1 + np.exp(-alpha * (x - a)))
 
 
 #Takes in an array of sigmoid finctions parameters [alpha, a, xmin, xmax] and returns an array of sigmoid functions x and y coordinates
 def sigmoidArray(sigmoidparams):
     results = []
-    print(sigmoidparams[0][4])
-    sigmoidparams = sigmoidparams.astype(str)
     for sigmoid in sigmoidparams:
-        direction, sigmoid = sigmoid[-1], sigmoid[:-1]
-        alpha, a, xmin, xmax = map(Decimal, sigmoid)
+        alpha, a, xmin, xmax, direction = sigmoid
         xcorr = np.arange(xmin, xmax + 1)
         ycorr = sigmoidPoint(alpha, a, xcorr)
         ycorr = alphaTransform(ycorr, direction)
         results.append([xcorr, ycorr])
-    return np.array(results)
+    return np.array(results, dtype=np.float64)
 
 
 #Transforms a point of a sigmoid function into a 1-0.5 , 0.5-0 form
 def alphaTransform(ycorrs, direction):
-    if direction == "1.0":
-        ycorrs = (Decimal(1) + ycorrs) / 2
+    if direction:
+        ycorrs = (1 + ycorrs) / 2
     else:
-        ycorrs = (Decimal(1) - ycorrs) / 2
+        ycorrs = (1 - ycorrs) / 2
     return ycorrs
 
 
@@ -49,11 +44,11 @@ def sigmoidAggregate(sigmoidTomb):
     #yTomb = np.array([sigmoidTomb[1]])
     yTomb = np.array([sigmoid[1] for sigmoid in sigmoidTomb])
 
-    preprod = (Decimal(1)-yTomb)/yTomb
+    preprod = (1-yTomb)/yTomb
 
     prod = np.prod(preprod, axis=0)
 
-    yAggregated = Decimal(1) / (Decimal(1) + prod)
+    yAggregated = 1 / (1 + prod)
 
     xAggregated = sigmoidTomb[0][0]
     return np.array([xAggregated, yAggregated])
@@ -61,10 +56,13 @@ def sigmoidAggregate(sigmoidTomb):
 
 #Show the aggregate of an array of sigmoid functions
 def aggregateShow(aggregateArray):
-    plt.figure().set_figheight(2.5)
-    aggregated = sigmoidAggregate(aggregateArray)
+    plt.figure().set_size_inches(14, 7)
 
-    plt.plot(aggregated[0], aggregated[1])
+    plt.plot(aggregateArray[0], aggregateArray[1])
+    plt.plot(aggregateArray[0][0], 0)
+    plt.plot(aggregateArray[0][0], 1)
+
+
     plt.grid()
     plt.show()
 
@@ -83,7 +81,6 @@ def sigmoidArrayAndAggregatePlot(sigmoidArray):
     fig.set_size_inches(14, 7)
 
     aggregated = sigmoidAggregate(sigmoidArray)
-    print(aggregated[1])
     ax1.plot(aggregated[0], aggregated[1])
     ax1.plot(0, 0)
     ax1.plot(0, 1)
@@ -102,7 +99,7 @@ def sigmoidArrayAndAggregatePlot(sigmoidArray):
 
 
 def createParamsArray(alpha, a, xmin, xmax, direction):
-    return np.array([[alpha, a, xmin, xmax, direction]])
+    return np.array([[alpha, a, xmin, xmax, direction]], dtype=np.float64)
 
 
 def appendParams(paramArray, alpha, a, xmin, xmax, direction):
@@ -113,25 +110,72 @@ def appendParams(paramArray, alpha, a, xmin, xmax, direction):
 #Takes a 2d array as parameter that consists of two arrays.
 #The first array is x values, the second is y values
 def aggMaxIndexes(aggregated):
-    return np.array(argrelextrema(aggregated[1], np.greater, mode='wrap')[0])
+    return np.array(find_peaks(aggregated[1])[0])
+    #return np.array(argrelextrema(aggregated[1], np.greater, mode='wrap')[0])
 
 
 #Takes a 2d array as parameter that consists of two arrays.
 #The first array is x values, the second is y values
 def aggMaxValues(aggregated):
-    return aggregated[1][aggMaxIndexes(aggregated)]
+    return np.array(aggregated[1][aggMaxIndexes(aggregated)])
 
 
 #Takes a 2d array as parameter that consists of two arrays.
 #The first array is x values, the second is y values
 def aggMinIndexes(aggregated):
-    return np.array(argrelextrema(aggregated[1], np.less, mode='wrap')[0])
+    return np.array(find_peaks(-aggregated[1])[0])
+    #return np.array(argrelextrema(aggregated[1], np.less, mode='wrap')[0])
 
 
 #Takes a 2d array as parameter that consists of two arrays.
 #The first array is x values, the second is y values
 def aggMinValues(aggregated):
-    return aggregated[1][aggMinIndexes(aggregated)]
+    return np.array(aggregated[1][aggMinIndexes(aggregated)])
+
+
+def aggMinMaxIndexesSorted(aggregated):
+    mins = aggMinIndexes(aggregated)
+    maxes = aggMaxIndexes(aggregated)
+    sortedindexes = None
+    if mins.size == 0 and maxes.size == 0:
+        if aggregated[1][0] == aggregated[1][-1]:
+            return None
+        else:
+            return np.array([0, aggregated[1].size-1])
+
+    sortedindexes = np.array([0, aggregated[1].size-1])
+    sortedindexes = np.append(sortedindexes, mins)
+    sortedindexes = np.append(sortedindexes, maxes)
+    sortedindexes = np.sort(sortedindexes)
+
+    return sortedindexes
+
+
+def subtractFirstSigmoidKnowingAlpha(inputaggregate, alpha):
+    returnaggregate = inputaggregate
+    minmaxes = aggMinMaxIndexesSorted(inputaggregate)
+    xmin = inputaggregate[0][0]
+    xmax = inputaggregate[0][-1]
+    inputaggregateY = np.array(inputaggregate[1])
+
+    inputprod = (1/inputaggregateY)-1
+
+    inputaggregateY[(0.499999 < inputaggregateY) & (inputaggregateY < 0.500001)] = 0.5
+    inputaggregateY = gaussian_filter1d(inputaggregateY, 4)
+
+    derrivates = np.gradient(np.gradient(inputaggregateY))
+    infls = np.where(1 < abs(np.diff(np.sign(derrivates))))[0]
+
+    subtractionParams = createParamsArray(alpha, infls[0]+xmin, xmin, xmax, minmaxes[0] < minmaxes[1])
+    subtractionSigmoid = sigmoidArray(subtractionParams)
+
+    preprod = (1-subtractionSigmoid[0][1]) / subtractionSigmoid[0][1]
+    prod = inputprod / preprod
+
+    returnaggregate[1] =inputaggregate[1] = 1 / (1 + prod)
+
+    return returnaggregate
+
 
 #Takes a 2d array as parameter that consists of two arrays.
 #The first array is x values, the second is y values
